4e3baaf1dc3eff1c249958b5e05fca740158274a
rodney <rodney@rodneykahan.es>
2019-10-05 16:13:10 -0400

    enemy attack animation working

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     private void Update()
     {
@@ -36,8 +37,7 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit this thing: " + hit.transform.name);
-            //TODO: add some hit effect for visual players
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;  //protects us if we hit something thats not an enemy
             target.TakeDamage(damage);
@@ -46,5 +46,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
     }
 }
