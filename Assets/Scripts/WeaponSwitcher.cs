90aa3561ba538d47278d35db7d12fb38b59425d6
rodney <rodney@rodneykahan.es>
2019-10-10 11:31:48 -0400

    added foundation for weapon switching

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] int currentWeapon = 0;
+
+    void Start()
+    {
+        SetWeaponActive();    
+    }
+
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ProcessScrollWheel()
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+    private void SetWeaponActive()
+    {
+        int weaponIndex = 0;
+
+        foreach (Transform weapon in transform)
+        {
+            if(weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);                
+            }
+
+            weaponIndex++;
+        }
+
+    }
+
+}
